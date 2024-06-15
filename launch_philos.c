1ae516902da47bca02eeb5c64821d93fb2b9e4ec
Semen Ganiev <sganiev@2-B-2.42heilbronn.de>
2024-06-15 21:25:39 +0200

    Added thread's joining

--- launch_philos.c
+++ launch_philos.c
@@ -6,7 +6,7 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 20:06:13 by sganiev           #+#    #+#             */
-/*   Updated: 2024/06/15 20:39:58 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/06/15 20:47:11 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,27 @@ void	*routine(void *data)
 {
 }
 
-void	launch_philos(t_program *data)
+int	launch_philos(t_program *data)
 {
 	int	i;
 
 	i = 0;
 	data->philos = (t_philo *)malloc(sizeof(t_philo) * data->philo_num);
-	if (data->philos != NULL)
+	if (data->philos == NULL)
+		return (1);
+	while (i < data->philo_num)
 	{
-		while (i < data->philo_num)
+		if (pthread_create(&data->philos[i++].th, NULL, &routine, data))
 		{
-			if (pthread_create(&data->philos[i++].th, NULL, &routine, data))
-			{
-				free_struct();
-				return (1);
-			}
+			free_struct();
+			return (1);
 		}
 	}
+	i = 0;
+	while (i < data->philo_num)
+	{
+		if (pthread_join(data->philos[i++].th, NULL))
+			return (1);
+	}
+	return (0);
 }
