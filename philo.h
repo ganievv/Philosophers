c33cb829ee2cdb62f36fd5b323ddc57ec6305743
Semen Ganiev <sganiev@2-B-2.42heilbronn.de>
2024-06-15 16:35:58 +0200

    Created header file

---
+++ philo.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/15 16:22:33 by sganiev           #+#    #+#             */
+/*   Updated: 2024/06/15 16:30:22 by sganiev          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <time.h>
+# include <errno.h>
+# include <semaphore.h>
+# include <stdbool.h>
+# include <time.h>
+
+int	ft_atoi(const char *str);
+
+#endif
