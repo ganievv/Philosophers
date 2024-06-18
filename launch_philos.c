d9fe4ca1e43d6222514c9f6b0ee5a1093f416c6c
Semen Ganiev <sganiev@2-B-2.42heilbronn.de>
2024-06-18 16:31:18 +0200

    Changed handling of philos and forks(from fixed max size to malloc)

--- launch_philos.c
+++ launch_philos.c
@@ -6,24 +6,30 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 20:06:13 by sganiev           #+#    #+#             */
-/*   Updated: 2024/06/17 16:32:27 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/06/18 16:26:58 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static void	philo_init(t_program *data, t_philo *philos)
+static int	philo_fork_init(t_program *data)
 {
 	int	i;
 
 	i = 0;
+	data->philos = (t_philo *)malloc(sizeof(t_philo) * data->philo_num);
+	data->forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t)
+			* data->philo_num);
+	if (!data->philos || !data->forks)
+		return (1);
 	while (i < data->philo_num)
 	{
-		philos[i].id = i;
-		philos[i].times_eaten = 0;
-		philos[i].prog_data = data;
+		data->philos[i].id = i;
+		data->philos[i].times_eaten = 0;
+		data->philos[i].prog_data = data;
 		i++;
 	}
+	return (0);
 }
 
 void	launch_philos(t_program *data, int	*error_flag)
@@ -31,7 +37,7 @@ void	launch_philos(t_program *data, int	*error_flag)
 	int	i;
 
 	i = -1;
-	philo_init(data, data->philos);
+	philo_fork_init(data);
 	while (++i < data->philo_num)
 	{
 		if (pthread_create(&data->philos[i].th, NULL,
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 16:22:33 by sganiev           #+#    #+#             */
-/*   Updated: 2024/06/17 16:30:53 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/06/18 16:23:54 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,31 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <pthread.h>
-# include <time.h>
 # include <errno.h>
 # include <semaphore.h>
 # include <stdbool.h>
-# include <time.h>
+# include <sys/time.h>
 
 # define MAX_PHILOSOPHERS 200
 
 typedef struct s_program
 {
-	t_philo	philos[MAX_PHILOSOPHERS];
-	int		philo_num;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	int		each_philo_must_eat_num;
+	t_philo			*philos;
+	pthread_mutex_t	*forks;
+	int				philo_num;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				each_philo_must_eat_num;
 }	t_program;
 
 typedef struct s_philo
 {
-	t_program	*prog_data;
-	pthread_t	th;
-	int			id;
-	int			times_eaten;
+	t_program		*prog_data;
+	pthread_t		th;
+	pthread_mutex_t	*fork;
+	int				id;
+	int				times_eaten;
 }	t_philo;
 
 int		ft_atoi(const char *str);
