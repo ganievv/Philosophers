0dac72f99b2ff7c9865fff06b8b4fecbad21134e
Semen Ganiev <sganiev@2-B-2.42heilbronn.de>
2024-06-19 16:18:10 +0200

    Divided fork's taking to 'take_fork()' and eating to 'eat()'

--- routine.c
+++ routine.c
@@ -6,42 +6,52 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 15:48:52 by sganiev           #+#    #+#             */
-/*   Updated: 2024/06/18 20:21:27 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/06/19 16:15:58 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-// if (philo->id + 1 % 2 != 0)
+static void	take_fork(pthread_mutex_t *fork, pthread_mutex_t *print_mutex,
+int id, struct timeval *start_time)
+{
+	struct timeval	current_time;
+
+	pthread_mutex_lock(fork);
+	pthread_mutex_lock(print_mutex);
+	gettimeofday(&current_time, NULL);
+	printf("%lld %d has taken a fork\n",
+		(current_time.tv_sec - start_time->tv_sec) * 1000
+		+ (current_time.tv_usec - start_time->tv_usec) / 1000,
+		id + 1);
+	pthread_mutex_unlock(print_mutex);
+}
+
+static void	eat(int id, pthread_mutex_t *print_mutex,
+struct timeval *start_time, int time_to_eat)
+{
+	struct timeval	current_time;
+
+	pthread_mutex_lock(print_mutex);
+	gettimeofday(&current_time, NULL);
+	printf("%lld %d is eating\n",
+		(current_time.tv_sec - start_time->tv_sec) * 1000
+		+ (current_time.tv_usec - start_time->tv_usec) / 1000,
+		id + 1);
+	ft_usleep(time_to_eat);
+	pthread_mutex_unlock(print_mutex);
+}
 
 static void	take_forks_and_eat(t_philo *philo)
 {
 	struct timeval	current_time;
 
-	pthread_mutex_lock(philo->left_fork);
-	pthread_mutex_lock(&philo->prog_data->print_mutex);
-	gettimeofday(&current_time, NULL);
-	printf("%lld %d has taken a fork\n",
-		(current_time.tv_sec - philo->prog_data->start_time.tv_sec) * 1000
-		+ (current_time.tv_usec - philo->prog_data->start_time.tv_usec) / 1000,
-		philo->id + 1);
-	pthread_mutex_unlock(&philo->prog_data->print_mutex);
-	pthread_mutex_lock(philo->right_fork);
-	pthread_mutex_lock(&philo->prog_data->print_mutex);
-	gettimeofday(&current_time, NULL);
-	printf("%lld %d has taken a fork\n",
-		(current_time.tv_sec - philo->prog_data->start_time.tv_sec) * 1000
-		+ (current_time.tv_usec - philo->prog_data->start_time.tv_usec) / 1000,
-		philo->id + 1);
-	pthread_mutex_unlock(&philo->prog_data->print_mutex);
-	pthread_mutex_lock(&philo->prog_data->print_mutex);
-	gettimeofday(&current_time, NULL);
-	printf("%lld %d is eating\n",
-		(current_time.tv_sec - philo->prog_data->start_time.tv_sec) * 1000
-		+ (current_time.tv_usec - philo->prog_data->start_time.tv_usec) / 1000,
-		philo->id + 1);
-	ft_usleep(philo->prog_data->time_to_eat);
-	pthread_mutex_unlock(&philo->prog_data->print_mutex);
+	take_fork(philo->left_fork, &philo->prog_data->print_mutex,
+		philo->id, &philo->prog_data->start_time);
+	take_fork(philo->right_fork, &philo->prog_data->print_mutex,
+		philo->id, &philo->prog_data->start_time);
+	eat(philo->id, &philo->prog_data->print_mutex,
+		&philo->prog_data->start_time, philo->prog_data->time_to_eat);
 	pthread_mutex_unlock(philo->left_fork);
 	pthread_mutex_unlock(philo->right_fork);
 }
