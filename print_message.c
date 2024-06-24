bf72631c4d78308c6184768e0f351a7dd376e318
Semen Ganiev <sganiev@2-B-2.42heilbronn.de>
2024-06-24 17:36:28 +0200

    Added checking of stop_flag before printing

--- print_message.c
+++ print_message.c
@@ -6,7 +6,7 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 18:12:55 by sganiev           #+#    #+#             */
-/*   Updated: 2024/06/20 18:14:22 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/06/24 16:35:02 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,14 @@ void	print_message(t_philo *philo, char *message)
 
 	pthread_mutex_lock(&philo->prog_data->print_mutex);
 	gettimeofday(&current_time, NULL);
-	printf("%ld %d %s\n",
-		((current_time.tv_sec * 1000000 + current_time.tv_usec)
-			- (philo->prog_data->start_time.tv_sec * 1000000
-				+ philo->prog_data->start_time.tv_usec))
-		/ 1000,
-		philo->id + 1, message);
+	if (!philo->prog_data->stop_flag)
+	{
+		printf("%ld %d %s\n",
+			((current_time.tv_sec * 1000000 + current_time.tv_usec)
+				- (philo->prog_data->start_time.tv_sec * 1000000
+					+ philo->prog_data->start_time.tv_usec))
+			/ 1000,
+			philo->id + 1, message);
+	}
 	pthread_mutex_unlock(&philo->prog_data->print_mutex);
 }
