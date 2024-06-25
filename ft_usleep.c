3565eb10da519d7e99afb7df5566163f315a1ba9
Semen Ganiev <sganiev@2-B-2.42heilbronn.de>
2024-06-25 17:07:58 +0200

    Fixed precision of the waiting

--- ft_usleep.c
+++ ft_usleep.c
@@ -6,7 +6,7 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 17:45:24 by sganiev           #+#    #+#             */
-/*   Updated: 2024/06/18 17:45:36 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/06/25 16:44:21 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,19 @@ void	ft_usleep(useconds_t usec)
 	struct timeval	start;
 	struct timeval	current;
 	long long		elapsed;
+	long long		rem;
 
 	elapsed = 0;
 	gettimeofday(&start, NULL);
-	while (elapsed < usec)
+	while (true)
 	{
 		gettimeofday(&current, NULL);
 		elapsed = (current.tv_sec - start.tv_sec) * 1000000LL
 			+ (current.tv_usec - start.tv_usec);
+		if (elapsed >= usec)
+			break ;
+		rem = usec - elapsed;
+		if (rem > 1000)
+			usleep(rem / 2);
 	}
 }
