287babea1c113087aa39b46847c3e166adc075df
Semen Ganiev <sganiev@3-B-2.42heilbronn.de>
2024-06-26 09:39:33 +0200

    Moved functions from deleted files to 'utils.c'

--- utils.c
+++ utils.c
@@ -1,16 +1,58 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_message.c                                    :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/20 18:12:55 by sganiev           #+#    #+#             */
-/*   Updated: 2024/06/24 16:35:02 by sganiev          ###   ########.fr       */
+/*   Created: 2024/06/26 09:33:06 by sganiev           #+#    #+#             */
+/*   Updated: 2024/06/26 09:35:56 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int	ft_atoi(const char *str)
+{
+	int	number;
+	int	i;
+	int	minus;
+
+	i = 0;
+	minus = 1;
+	number = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+		minus = -1;
+	if (minus == -1 || str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+		number = (number * 10) + (str[i++] - '0');
+	return (number * minus);
+}
+
+void	ft_usleep(useconds_t usec)
+{
+	struct timeval	start;
+	struct timeval	current;
+	long long		elapsed;
+	long long		rem;
+
+	elapsed = 0;
+	gettimeofday(&start, NULL);
+	while (true)
+	{
+		gettimeofday(&current, NULL);
+		elapsed = (current.tv_sec - start.tv_sec) * 1000000LL
+			+ (current.tv_usec - start.tv_usec);
+		if (elapsed >= usec)
+			break ;
+		rem = usec - elapsed;
+		if (rem > 1000)
+			usleep(rem / 2);
+	}
+}
 
 void	print_message(t_philo *philo, char *message)
 {
