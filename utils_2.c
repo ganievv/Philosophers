aea054e6eed276d6410452fbb5bf36f076be92ea
Semen Ganiev <sganiev@3-B-8.42heilbronn.de>
2024-06-27 17:05:35 +0200

    Added getters and setters; moved threads activation and monitoring creations here

--- utils_2.c
+++ utils_2.c
@@ -6,20 +6,76 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 08:51:21 by sganiev           #+#    #+#             */
-/*   Updated: 2024/06/27 08:58:44 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/06/27 16:26:09 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+bool	get_bool_var(pthread_mutex_t *mutex, bool *value)
+{
+	bool	buf;
+
+	pthread_mutex_lock(mutex);
+	buf = *value;
+	pthread_mutex_unlock(mutex);
+	return (buf);
+}
+
+void	set_bool_var(pthread_mutex_t *mutex, bool value, bool *dst)
+{
+	pthread_mutex_lock(mutex);
+	*dst = value;
+	pthread_mutex_unlock(mutex);
+}
+
+unsigned long long	get_ullong_var(pthread_mutex_t *mutex,
+	unsigned long long *value)
+{
+	unsigned long long	buf;
+
+	pthread_mutex_lock(mutex);
+	buf = *value;
+	pthread_mutex_unlock(mutex);
+	return (buf);
+}
+
+void	set_ullong_var(pthread_mutex_t *mutex,
+	unsigned long long value, unsigned long long *dst)
+{
+	pthread_mutex_lock(mutex);
+	*dst = value;
+	pthread_mutex_unlock(mutex);
+}
+
+long	get_long_var(pthread_mutex_t *mutex, long *value)
+{
+	long	buf;
+
+	pthread_mutex_lock(mutex);
+	buf = *value;
+	pthread_mutex_unlock(mutex);
+	return (buf);
+}
+
+void	set_long_var(pthread_mutex_t *mutex, long value, long *dst)
+{
+	pthread_mutex_lock(mutex);
+	*dst = value;
+	pthread_mutex_unlock(mutex);
+}
+
 void	activate_threads_and_monitor(t_program *data, int *err_flag)
 {
-	data->start_time = take_time(MILLISECONDS);
-	data->is_ready = 1;
+	if (*err_flag)
+		return ;
+	set_ullong_var(&data->prog_data_mutex, take_time(MILLISECONDS),
+		&data->start_time);
+	set_bool_var(&data->prog_data_mutex, 1, &data->is_ready);
 	if (pthread_create(&data->th_monitoring, NULL,
 			monitoring, data) != 0)
 	{
-		data->stop_flag = 1;
+		set_bool_var(&data->prog_data_mutex, 1, &data->stop_flag);
 		*err_flag = 1;
 	}
 }
