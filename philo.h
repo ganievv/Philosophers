d0401a991e0c3172dba4adccf2a6b6804bac7de2
Semen Ganiev <sganiev@3-B-8.42heilbronn.de>
2024-06-27 17:08:46 +0200

    Added new func prototypes, mutexes for philo and prog_data; changed data type of few var-s

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 16:22:33 by sganiev           #+#    #+#             */
-/*   Updated: 2024/06/26 16:33:43 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/06/27 16:57:16 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,11 @@
 # include <sys/time.h>
 # include <limits.h>
 
-# define MILLISECONDS 1
-# define MICROSECONDS 2
+typedef enum e_time_units
+{
+	MILLISECONDS,
+	MICROSECONDS,
+}	t_time_units;
 
 typedef struct s_philo	t_philo;
 
@@ -33,6 +36,7 @@ typedef struct s_program
 	t_philo				*philos;
 	pthread_mutex_t		*forks;
 	pthread_mutex_t		print_mutex;
+	pthread_mutex_t		prog_data_mutex;
 	pthread_t			th_monitoring;
 	unsigned long long	start_time;
 	long				philo_num;
@@ -50,8 +54,9 @@ struct s_philo
 	pthread_t			th;
 	pthread_mutex_t		*left_fork;
 	pthread_mutex_t		*right_fork;
+	pthread_mutex_t		philo_mutex;
 	int					id;
-	int					times_eaten;
+	long				times_eaten;
 	unsigned long long	last_meal_time;
 };
 
@@ -61,6 +66,18 @@ void				*routine(void *data);
 void				ft_usleep(useconds_t usec);
 void				print_message(t_philo *philo, char *message);
 void				*monitoring(void *data);
-unsigned long long	take_time(int type);
+unsigned long long	take_time(t_time_units type);
+void				activate_threads_and_monitor(t_program *data,
+						int *err_flag);
+
+bool				get_bool_var(pthread_mutex_t *mutex, bool *value);
+void				set_bool_var(pthread_mutex_t *mutex, bool value, bool *dst);
+unsigned long long	get_ullong_var(pthread_mutex_t *mutex,
+						unsigned long long *value);
+void				set_ullong_var(pthread_mutex_t *mutex,
+						unsigned long long value, unsigned long long *dst);
+long				get_long_var(pthread_mutex_t *mutex, long *value);
+void				set_long_var(pthread_mutex_t *mutex, long value, long *dst);
+
 
 #endif
