366a1c31fcbe1b157d224d6c04e05254ec6095f5
Semen Ganiev <sganiev@2-B-2.42heilbronn.de>
2024-08-01 22:54:46 +0200

    moved mutex init to handle_one_philo() from routine_one_philo()

--- src/one_philo.c
+++ src/one_philo.c
@@ -6,7 +6,7 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 22:08:32 by sganiev           #+#    #+#             */
-/*   Updated: 2024/08/01 22:26:29 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/08/01 22:52:35 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,35 @@
 
 void	*routine_one_philo(void *data)
 {
-	t_program		*prog_d;
-	pthread_mutex_t	fork;
+	t_program	*prog_d;
 
 	prog_d = (t_program *)data;
-	pthread_mutex_init(&fork, NULL);
-	pthread_mutex_lock(&fork);
+	pthread_mutex_lock(&prog_d->forks[0]);
 	printf("0 1 has taken a fork\n");
 	ft_usleep(prog_d->time_to_die_us);
 	printf("%ld 1 died\n", prog_d->time_to_die_us / 1000);
-	pthread_mutex_unlock(&fork);
-	pthread_mutex_destroy(&fork);
+	pthread_mutex_unlock(&prog_d->forks[0]);
 	return (NULL);
+}
+
+static void	free_alloc(t_program *data)
+{
+	pthread_mutex_destroy(&data->forks[0]);
+	free(data->forks);
 }
 
 void	handle_one_philo(t_program *data)
 {
 	pthread_t	th;
 
+	data->forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t)
+			* data->philo_num);
+	if (!data->forks)
+		return ;
+	pthread_mutex_init(&data->forks[0], NULL);
 	if (pthread_create(&th, NULL, routine_one_philo, data) != 0)
-		return ;
+		return (free_alloc(data), (void)0);
 	if (pthread_join(th, NULL) != 0)
-		return ;
+		return (free_alloc(data), (void)0);
+	free_alloc(data);
 }
